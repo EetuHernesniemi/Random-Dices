3e8e450dd54050ded1413b879bc669583b68766a
--local <--local>
2021-09-13 15:44:38 +0300

    referencing image views in the view controller. Swift viewDidLoad functionality to imageview attributes.

--- Random-Dices/ViewController.swift
+++ Random-Dices/ViewController.swift
@@ -1,10 +1,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
+        
+        diceImageViewOne.alpha = 0.5
+        
     }
 
 
